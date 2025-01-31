8de533e812ea188124eaa7f893cb379ecba12571
Downcasting <hoo9565j@gmail.com>
2025-01-31 19:01:19 +0900

    doing

---
+++ 11_Cross_Entropy.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+'''
+Cross Entropy Loss
+CE(y, y_hat) = - sum(y_i * log(y_hat_i))
+'''
+
+def cross_entropy(actual, predicted):
+    loss = -np.sum(actual * np.log(predicted))
+    return loss
+
+# y must be one hot encoded
+# if class 0: [1 0 0]
+# if class 1: [0 1 0]
+# if class 2: [0 0 1]
+Y = np.array([1, 0, 0])
+
+# y_hat are the predicted probabilities
+Y_pred_good = np.array([0.7, 0.2, 0.1])
+Y_pred_bad = np.array([0.1, 0.3, 0.6])
+l1 = cross_entropy(Y, Y_pred_good)
+l2 = cross_entropy(Y, Y_pred_bad)
+print(f'Loss1 numpy: {l1:.4f}')
+print(f'Loss2 numpy: {l2:.4f}')
