96720aa57a90657ca137999400f55fa913985f65
Downcasting <hoo9565j@gmail.com>
2025-02-13 23:21:10 +0900

    fds

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 from utils import ALL_LETTERS, N_LETTERS
 from utils import load_data, letter_to_tensor, line_to_tensor, random_training_example
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using device: {device}')
 
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
@@ -25,15 +27,15 @@ class RNN(nn.Module):
         return output, hidden
     
     def init_hidden(self):
-        return torch.zeros(1, self.hidden_size)
+        return torch.zeros(1, self.hidden_size, device=device)
     
 category_lines, all_categories = load_data()
 n_categories = len(all_categories)
 
 n_hidden = 128
-rnn = RNN(N_LETTERS, n_hidden, n_categories)
+rnn = RNN(N_LETTERS, n_hidden, n_categories).to(device)
 
-input_tensor = line_to_tensor('Albert')
+input_tensor = line_to_tensor('Albert').to(device)
 hidden_tensor = rnn.init_hidden()
 
 output, next_hidden = rnn(input_tensor[0], hidden_tensor)
@@ -42,3 +44,70 @@ output, next_hidden = rnn(input_tensor[0], hidden_tensor)
 def category_from_output(output):
     category_idx = torch.argmax(output).item()
     return all_categories[category_idx]
+
+print(category_from_output(output))
+
+criterion = nn.NLLLoss() # negative log-likelihood loss
+learning_rate = 0.005
+optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
+
+def train(line_tensor, category_tensor):
+    hidden = rnn.init_hidden()
+    
+    rnn.zero_grad()
+
+    line_tensor = line_tensor.to(device)
+    category_tensor = category_tensor.to(device)
+    
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+        
+    loss = criterion(output, category_tensor)
+    loss.backward()
+    
+    optimizer.step()
+    
+    return output, loss.item()
+
+current_loss = 0
+all_losses = []
+plot_steps, print_steps = 1000, 5000
+n_iters = 100000
+for i in range(n_iters):
+    category, line, category_tensor, line_tensor = random_training_example(category_lines, all_categories)
+
+    output, loss = train(line_tensor, category_tensor)
+    current_loss += loss
+    
+    if (i + 1) % plot_steps == 0:
+        all_losses.append(current_loss / plot_steps)
+        current_loss = 0
+
+    if (i + 1) % print_steps == 0:
+        guess = category_from_output(output)
+        correct = '✓' if guess == category else f'✗ ({category})'
+        print(f'{i + 1} {i / n_iters * 100:.2f}% ({loss:.4f}) {line} / {guess} {correct}')
+
+plt.figure()
+plt.plot(all_losses)
+plt.show()
+
+def predict(input_line):
+    print(f'\n> {input_line}')
+    with torch.no_grad():
+        line_tensor = line_to_tensor(input_line).to(device)
+        hidden = rnn.init_hidden()
+        
+        for i in range(line_tensor.size()[0]):
+            output, hidden = rnn(line_tensor[i], hidden)
+        
+        guess = category_from_output(output)
+        print(guess)
+
+
+while True:
+    sentence = input('Input: ')
+    if sentence == 'quit':
+        break
+
+    predict(sentence)
