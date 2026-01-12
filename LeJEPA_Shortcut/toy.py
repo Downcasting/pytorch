f72d03467b0613c6768623ff841dcd80d5aee71e
Desktop <hoo9565j@hanyang.ac.kr>
2026-01-12 22:42:12 +0900

    SIGReg success

---
+++ LeJEPA_Shortcut/toy.py
@@ -0,0 +1,16 @@
+import torch
+import lejepa
+
+embeddings = torch.ones(256, 768, requires_grad=True)
+
+univariate_test = lejepa.univariate.EppsPulley(n_points=17)
+
+loss_fn = lejepa.multivariate.SlicingUnivariateTest(
+    univariate_test=univariate_test,
+    num_slices=1024
+)
+
+loss = loss_fn(embeddings)
+loss.backward()
+
+print(loss.item())
